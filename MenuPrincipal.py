fa1140063fe74c494ca0209be035d12dbe751775
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-04-28 11:52:24 -0500

    Actualizacion

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -1,15 +1,16 @@
 import PySimpleGUI as sg
 
-logo = "logo.png"
 
 layout = [
-    [sg.Image(image_filename=logo)],
-    [sg.Text("Bienvenido a la tecnoacademia si es tu primera vez usando el sistema de asistencia por favor seleccione, 'Registro nuevo' de lo contrario 'Enviar Asistencia'")],
-    [sg.Button('Registro nuevo'), sg.Button('Enviar Asistencia')],
-    [sg.Text('', size=(30, 3), font=('Helvetica', 18), text_color='red', key='out')]]
+    [sg.Image(filename='logo.png')],
+    [sg.Text('Bienvenido', font=('Helvetica', 25), justification='center')],
+    [sg.Text("*Si es tu primera vez usando el sistema de asistencia por favor seleccione, 'Registro nuevo' de lo contrario 'Enviar Asistencia'", font=('Helvetica', 12), text_color='blue')],
+    [sg.Button('Registro nuevo', font=('Helvetica', 18)), sg.Button('Enviar Asistencia', font=('Helvetica', 18))],
+    [sg.Text('', size=(20, 1), font=('Helvetica', 18), text_color='red', key='out')]]
 
 window = sg.Window('Tecnoacademia', layout, default_button_element_size=(
     15, 2), auto_size_buttons=False, element_justification='c')
+window.Maximize
 
 event, values = window.read()    
 window.close()
--- helloword.py
+++ helloword.py
@@ -7,13 +7,16 @@ def sumar(a, b):
 
 layout = [[sg.Text('Filename')],
           [sg.Text("numero 1"), sg.Input(), sg.Text("numero 2"), sg.Input()],
-          [sg.OK(), sg.Cancel()]]
+          [sg.OK(), sg.Cancel()],
+          [sg.Image(filename='logo.png')]]
 
-window = sg.Window('Get filename example', layout)
+window = sg.Window('Pruebas funciones', layout)
 
-event, values = window.read()
-if event == 'OK':
-    sumar(values[0], values[1])
-    
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'OK':
+        sg.popup_ok(sumar(int(values[0]), int(values[1])))
 
 window.close()
